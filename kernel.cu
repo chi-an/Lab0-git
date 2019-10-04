deb1c16a13a7d331729e038ad01100a75ff5f864
Mohammadreza Rezvani <41197372+MohammadrezaRezvani@users.noreply.github.com>
2019-10-03 19:09:49 -0700

    Initial commit

---
+++ kernel.cu
@@ -0,0 +1,37 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#include <stdio.h>
+    
+
+__global__ void VecAdd(int n, const float *A, const float *B, float* C) {
+
+    /********************************************************************
+     *
+     * Compute C = A + B
+     *   where A is a (1 * n) vector
+     *   where B is a (1 * n) vector
+     *   where C is a (1 * n) vector
+     *
+     ********************************************************************/
+
+    // INSERT KERNEL CODE HERE
+}
+
+
+void basicVecAdd( float *A,  float *B, float *C, int n)
+{
+
+    // Initialize thread block and kernel grid dimensions ---------------------
+
+    const unsigned int BLOCK_SIZE = 256; 
+
+    //INSERT CODE HERE
+
+}
+
